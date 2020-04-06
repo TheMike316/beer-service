71237c84bf38899c9605d2375a54df20c8bb1384
mike <hornegger@view-elevator.com>
2020-04-06 11:43:34 +0200

    Evict caches in test

--- src/test/java/com/example/beerservice/service/BeerServiceCachingTest.java
+++ src/test/java/com/example/beerservice/service/BeerServiceCachingTest.java
@@ -5,16 +5,19 @@ import com.example.beerservice.repository.BeerRepository;
 import com.example.beerservice.service.inventory.BeerInventoryService;
 import com.example.beerservice.web.model.BeerDto;
 import com.example.beerservice.web.model.BeerStyle;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.cache.CacheManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.math.BigDecimal;
 import java.util.Collections;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -34,6 +37,9 @@ public class BeerServiceCachingTest {
 
     @Autowired
     BeerService service;
+
+    @Autowired
+    CacheManager cacheManager;
 
     UUID id = UUID.randomUUID();
 
@@ -57,6 +63,11 @@ public class BeerServiceCachingTest {
             .price(BigDecimal.valueOf(6.99))
             .quantityOnHand(10000)
             .build();
+
+    @BeforeEach
+    void setUp() {
+        cacheManager.getCacheNames().forEach(c -> Objects.requireNonNull(cacheManager.getCache(c)).clear());
+    }
 
     @Test
     void getByIdCached() {

240187a0af1f2afc6c2b99c447ecb321c01847bd
mike <hornegger@view-elevator.com>
2020-04-11 11:24:54 +0200

    fix jackson stuff

--- src/main/java/com/example/beerservice/event/BrewBeerEvent.java
+++ src/main/java/com/example/beerservice/event/BrewBeerEvent.java
@@ -1,7 +1,9 @@
 package com.example.beerservice.event;
 
 import com.example.beerservice.web.model.BeerDto;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 public class BrewBeerEvent extends BeerEvent {
 
     public BrewBeerEvent(BeerDto beerDto) {
--- src/main/java/com/example/beerservice/event/NewInventoryEvent.java
+++ src/main/java/com/example/beerservice/event/NewInventoryEvent.java
@@ -1,7 +1,9 @@
 package com.example.beerservice.event;
 
 import com.example.beerservice.web.model.BeerDto;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 public class NewInventoryEvent extends BeerEvent {
 
     public NewInventoryEvent(BeerDto beerDto) {
